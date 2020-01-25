18526e70bd3f190206c83440aa391f3c82000803
Boston Brooks <39231018+BostonBrooks@users.noreply.github.com>
2020-01-25 10:15:36 +1000

    Add files via upload
    left click to create a circle, right click to delete a circle, left arrow to undo, right arrow to redo.

---
+++ Undo Redo/commands.h
@@ -0,0 +1,233 @@
+#define COMMANDS
+
+#ifndef POOLS
+#include "pools.h"
+#endif
+
+typedef struct {
+
+    int Pool_Self;
+    int Pool_Prev;
+    int Pool_Next;
+    int Pool_In_Use;
+
+    int List_Prev;
+    int List_Next;
+    int Type;
+    int Function;
+    int Data[16];
+
+} Command;
+
+#define UNDO 0
+#define REDO 1
+
+#define CREATE 0
+#define DELETE 1
+
+DECLARE_POOL(Command, 1028, 1028);
+DECLARE_LIST_OPS(Command, List);
+
+
+int (*Command_Functions[16])(int);
+
+int Undo_Head = -1;
+int Undo_Tail = -1;
+int Redo_Head = -1;
+int Redo_Tail = -1;
+
+int Init_Commands(void);
+int Clear_Redo_List(void);
+int Create_Circle_Event(int i, int j);
+int Command_Register(int);
+int Command_Unregister(int);
+int Command_Undo(void);
+int Command_Redo(void);
+
+int Command_Undo(void){
+
+    if (Undo_Tail != -1){
+        assert(Undo_Head != -1);
+        Command* Comd = Commands(Undo_Tail);
+        Command_Functions[Comd->Function](Undo_Tail);
+        
+    }
+
+    return 0;
+}
+
+int Command_Redo(void){
+
+    if (Redo_Tail != -1){
+        assert(Redo_Head != -1);
+        Command* Comd = Commands(Redo_Tail);
+        Command_Functions[Comd->Function](Redo_Tail);
+        
+    }
+
+    return 0;
+}
+
+
+int Command_Create_Circle(int Command_Int){
+
+//unregister command
+    Command* Object = Commands(Command_Int);
+//printf("Create %d, %d, %d \n", Object->List_Prev, Object->Pool_Self, Object->List_Next); fflush(stdout);
+
+    Command_Unregister(Command_Int);
+
+//change environment
+    int Circle_Int = Object->Data[0];
+    int Circle_x = Object->Data[1];
+    int Circle_y = Object->Data[2];
+
+    Create_Circle(Circle_x, Circle_y, Circle_Int);
+
+    Circle* Circ = Circles(Circle_Int);
+
+
+//printf("Circ Created %d, %d, %d \n", Circ->List_Prev, Circ->Pool_Self, Circ->List_Next); fflush(stdout);
+
+
+    
+   // printf("% %d, %d, %d \n", Circ->List_Prev, Circ->Pool_Self, Circ->List_Next);
+
+    
+//update command
+
+    if (Object->Type == UNDO){
+        Object->Type = REDO;
+    } else {
+        Object->Type = UNDO;
+    }
+    Object->Function = DELETE;
+//register command
+    Command_Register(Command_Int);
+}
+
+int Command_Delete_Circle(int Command_Int){
+
+//unregister command
+    Command* Object = Commands(Command_Int);
+
+//printf("Delete %d, %d, %d \n", Object->List_Prev, Object->Pool_Self, Object->List_Next); fflush(stdout);
+
+    Command_Unregister(Command_Int);
+
+//Update Command
+    int Circle_Int = Object->Data[0];
+
+    Circle* Circ = Circles(Circle_Int);
+    Object->Data[1] = Circ->x; 
+    Object->Data[2] = Circ->y;
+
+    if (Object->Type == UNDO){
+        Object->Type = REDO;
+    } else {
+        Object->Type = UNDO;
+    }
+    Object->Function = CREATE;
+
+//change environment
+    Delete_Circle(Circle_Int);
+
+
+//register command
+    Command_Register(Command_Int);
+
+
+}
+
+
+int Init_Commands(void){
+
+    Command_Make_NULL();
+    Command_Functions[CREATE] = &Command_Create_Circle;
+    Command_Functions[DELETE] = &Command_Delete_Circle;
+
+}
+
+int Clear_Redo_List(void){
+
+    int Object_Int = Command_List_Pop_Tail(&Redo_Head, &Redo_Tail);
+    while (Object_Int != -1){
+        Command_Delete(Object_Int);
+        Object_Int = Command_List_Pop_Tail(&Redo_Head, &Redo_Tail);
+
+    }
+
+}
+
+int Create_Circle_Event(int i, int j){
+
+    //create circle
+    int Circle_Int = Create_Circle(i, j, NEXT_AVAILABLE);
+
+    //add command to end of undo list
+
+    int Command_Int = Command_New(NEXT_AVAILABLE);
+    Command* Object = Commands(Command_Int);
+
+
+    Object->Function = 1;
+    Object->Data[0] = Circle_Int;
+    Object->Type = UNDO;
+
+    Command_Register(Command_Int);
+
+    //clear redo list
+    Clear_Redo_List();
+}
+
+int Delete_Circle_Event(int Circle_Int){
+
+    int Command_Int = Command_New(NEXT_AVAILABLE);
+    Command* Object = Commands(Command_Int);
+    Circle* Circle_Address = Circles(Circle_Int);
+
+    Object->Function = CREATE;
+    Object->Type = UNDO;
+    Object->Data[0] = Circle_Int;
+    Object->Data[1] = Circle_Address->x;
+    Object->Data[2] = Circle_Address->y;
+
+    Command_Register(Command_Int);
+
+    
+    Delete_Circle(Circle_Int);
+
+    return 0;
+}
+
+
+int Command_Register(int Command_Int){
+    Command* Object = Commands(Command_Int);
+//    Command* Prev;
+
+    if (Object->Type == UNDO){
+//add to undo tail
+        Command_List_Push_Tail(&Undo_Head, &Undo_Tail, Command_Int);
+
+
+    } else {
+//add to redo tail
+        Command_List_Push_Tail(&Redo_Head, &Redo_Tail, Command_Int);
+
+    }
+
+}
+
+int Command_Unregister(int Command_Int){
+
+    Command* Object = Commands(Command_Int);
+
+    if (Object->Type == UNDO){
+        Command_List_Remove(&Undo_Head, &Undo_Tail, Command_Int);
+
+    } else {
+        Command_List_Remove(&Redo_Head, &Redo_Tail, Command_Int);
+
+
+    }
+}
---
+++ Undo Redo/undoredo.c
@@ -0,0 +1,132 @@
+
+#include <stdlib.h>
+#include <stddef.h>
+#include <assert.h>
+#include <stdio.h>
+#include <unistd.h>
+
+
+#define DB(X) printf(" %d", X); fflush(stdout);
+
+#ifndef CIRCLES
+#include "circles.h"
+#endif
+
+#ifndef COMMANDS
+#include "commands.h"
+#endif
+
+int Handle_Input(void);
+int Command_List_All(void);
+int Circle_List_All(void);
+
+int main(void){
+
+    Init_Circles();
+    Init_Commands();
+
+    while(1){
+        Handle_Input();
+
+Command_List_All();
+printf("undo head: %d, undo Tail %d, redo_head %d, redo tail %d\n", Undo_Head, Undo_Tail, Redo_Head, Redo_Tail);  printf("\n");
+Circle_List_All();
+printf("List head: %d, Tail %d\n, available head: %d, Tail %d, in use head: %d, Tail %d", Circles_Head, Circles_Tail, Circle_Available_Head, Circle_Available_Tail, Circle_In_Use_Head, Circle_In_Use_Tail);  printf("\n");
+
+        Draw_Circles();
+
+    }
+}
+
+sfEvent event;
+
+int Handle_Input(void){
+
+    sfVector2i position;
+    while(1){
+        sfRenderWindow_waitEvent(window, &event);
+        
+        if (event.type == sfEvtMouseMoved){
+            if (!(sfMouse_isButtonPressed (sfMouseLeft))) continue;
+            if (event.mouseMove.x < 0) continue;
+            if (event.mouseMove.x > 800) continue;
+            if (event.mouseMove.y < 0) continue;
+            if (event.mouseMove.y > 600) continue;
+        
+            Create_Circle_Event(event.mouseMove.x, event.mouseMove.y);
+            return 0;
+
+        }
+
+        if (event.type == sfEvtMouseButtonPressed){
+            if (event.mouseButton.button == sfMouseLeft){
+                if (event.mouseButton.x < 0) continue;
+                if (event.mouseButton.x > 800) continue;
+                if (event.mouseButton.y < 0) continue;
+                if (event.mouseButton.y > 600) continue;
+        
+                Create_Circle_Event(event.mouseButton.x, event.mouseButton.y);
+                return 0;
+
+            } else if (event.mouseButton.button == sfMouseRight){
+DB(1);
+                if (event.mouseButton.x < 0) continue;
+                if (event.mouseButton.x > 800) continue;
+                if (event.mouseButton.y < 0) continue;
+                if (event.mouseButton.y > 600) continue;
+
+printf("mouse x = %d, mouse y = %d", event.mouseButton.x, event.mouseButton.y);fflush(stdout);
+        
+                int Circle_Int = Find_Nearest_Circle(
+                    event.mouseButton.x,
+                    event.mouseButton.y
+                );
+printf("Circle_Int, %d", Circle_Int);fflush(stdout);
+                if (Circle_Int >= 0) {
+                    Delete_Circle_Event(Circle_Int);
+                    return 0;
+                }
+            continue;
+            }
+
+        continue;
+
+        }
+
+        if (event.type == sfEvtKeyPressed){
+            if (event.key.code == sfKeyLeft){
+
+                Command_Undo();
+
+                return 0;
+            } else if (event.key.code == sfKeyRight){
+                Command_Redo();
+                return 0;
+            } else {
+                continue;
+            }
+
+        }
+
+    }
+    
+   
+}
+
+
+
+    int Command_List_All(void){
+        int j;
+            for (j = 0; j < 4; j++){
+                Command* Object = Commands_sudo(j);
+                printf("command, pool prev %d, self %d, next,  %d, in use %d, List prev %d, next %d\n", Object->Pool_Prev, Object->Pool_Self, Object->Pool_Next, Object->Pool_In_Use, Object->List_Prev, Object->List_Next); fflush(stdout);
+        }
+    }
+
+    int Circle_List_All(void){
+        int j;
+            for (j = 0; j < 16; j++){
+                Circle* Object = Circles_sudo(j);
+                printf("Circle, pool prev %d, self %d, next,  %d, in use %d, List prev %d, next %d\n", Object->Pool_Prev, Object->Pool_Self, Object->Pool_Next, Object->Pool_In_Use, Object->List_Prev, Object->List_Next); fflush(stdout);
+        }
+    }
